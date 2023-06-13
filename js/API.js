3103cfbc7e2968cc3ce80a86a7f9b478c436cf3c
Juan Demera <jpdemera@espol.edu.ec>
2023-06-13 10:02:16 -0500

    acceder a la data

--- js/API.js
+++ js/API.js
@@ -1,13 +1,30 @@
 (
     function () {
-        let URL = 'https://api.open-meteo.com/v1/forecast?latitude=-2.10&longitude=-79.98&hourly=temperature_2m';
+        let URL = 'https://api.open-meteo.com/v1/forecast?latitude=-2.20&longitude=-79.89&hourly=temperature_2m';
         fetch(URL)
             .then(response => response.json())
             .then(data => {
                 let timezone = data["timezone"]
                 let timezoneHTML = document.getElementById("timezone")
                 timezoneHTML.textContent = timezone;
+
+                let timezone_abbreviation = data["timezone_abbreviation"]
+                let timezone_abbreviationHTML = document.getElementById("gmt")
+                timezone_abbreviationHTML.textContent = timezone_abbreviation;
+
+                let elevation = data["elevation"]
+                let elevationHTML = document.getElementById("elevation")
+                elevationHTML.textContent = elevation;
+
+               
             })
             .catch(console.error);
     }
 )();
+
+setInterval(function() {
+    let fecha = new Date();
+    let horaLocal = fecha.toLocaleTimeString();
+    let horalocalHTML = document.getElementById("horaLocal")
+    horalocalHTML.textContent = horaLocal;
+  }, 1000);
